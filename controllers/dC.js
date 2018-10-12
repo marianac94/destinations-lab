01f3be64099707456718ecdffcfb4125bda41d7a
Mariana <marianac94@hotmail.com>
2018-10-12 17:04:44 -0500

    not right

--- controllers/dC.js
+++ controllers/dC.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+// calling the model that the controller need.
 const Destinations = require('../model/destinations');
 
+
+// show all destinations.
 router.get('/', (req, res) => {
-  res.send('Its aliveeeee')
+  Destinations.find({}, (err, allDestinations) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(allDestinations);
+      res.render('index.ejs', {
+        destinations: Destinations
+      });
+    }
+  });
 });
+
+
+// create new destination.
+router.post('/', (req, res) => {
+  Destinations.create({
+    name: 'Tulum',
+    fly: true,
+    Climate: 'hot',
+    image: 'https://exp.cdn-hotels.com/hotels/5000000/4520000/4515800/4515733/4c6016ed_z.jpg'
+  }, (err, createdDestinations) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(createdDestinations);
+      res.redirect('/destinations')
+    }
+  })
+});
+
 
 
 module.exports = router;
